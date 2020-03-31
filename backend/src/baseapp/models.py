2e5f2f8d170ccfdfa8b22417c2f35b0c0ed64943
Rajesh Golani <rajeshgolani@gmail.com>
2020-03-31 12:33:42 +0530

    Added additional fields for the forms

--- backend/src/baseapp/models.py
+++ backend/src/baseapp/models.py
@@ -27,10 +27,12 @@ class Covid(models.Model):
 class Context(models.Model):
     """This is the basic class for Aparment"""
     name = models.CharField(max_length=256)
+    contact_numbers = models.CharField(max_length=1024, null=True, blank=True)
     phone = models.BigIntegerField(blank=True, null=True)
     description = models.TextField(blank=True, null=True)
     how_many_people = models.IntegerField(blank=True, null=True)
-    what_help = models.TextField(blank=True, null=True)
+    what_help = models.CharField(max_length=1024, blank=True, null=True)
+    how_urgent = models.CharField(max_length=1024, null=True, blank=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True,
                                    blank=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True,
@@ -39,6 +41,8 @@ class Context(models.Model):
                                    blank=True)
     native_longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True,
                                    blank=True)
+    who_are_you = models.CharField(max_length=1024, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
