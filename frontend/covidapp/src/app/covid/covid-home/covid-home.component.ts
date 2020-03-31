9dfe1699fcd66ff4199481b4c93115920a753831
Mayank Rungta <mr.mynk@gmail.com>
2020-03-31 13:56:54 +0000

    Updated Home page to show the map and current stats

--- frontend/covidapp/src/app/covid/covid-home/covid-home.component.ts
+++ frontend/covidapp/src/app/covid/covid-home/covid-home.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-covid-home',
-  templateUrl: './covid-home.component.html',
-  styleUrls: ['./covid-home.component.css']
+    selector: 'app-covid-home',
+    templateUrl: './covid-home.component.html',
+    styleUrls: ['./covid-home.component.css']
 })
 export class CovidHomeComponent implements OnInit {
+    showMap: Boolean = true;
+    relief: Number = 943;
+    volunteers: Number = 102;
+    crisis: Number = 450;
+    constructor() {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 }
