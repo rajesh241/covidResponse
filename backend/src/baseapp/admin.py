cdbad661605973b35dafeff001dc11abe4d2bce3
Rajesh Golani <rajeshgolani@gmail.com>
2020-04-03 05:03:14 +0530

    Added feedback API

--- backend/src/baseapp/admin.py
+++ backend/src/baseapp/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Entity, Covid
+from .models import Entity, Covid, Feedback
+class FeedbackModelAdmin(admin.ModelAdmin):
+    """Model Adminf or class Location"""
+    list_display = ["id", "entity", "user"]
+    list_filter = ["user"]
+    search_fields = ["user"]
 class EntityModelAdmin(admin.ModelAdmin):
     """Model Adminf or class Location"""
     list_display = ["id", "name", "latitude", "longitude"]
@@ -13,3 +18,4 @@ class CovidModelAdmin(admin.ModelAdmin):
     search_fields = ["name"]
 admin.site.register(Covid, CovidModelAdmin)
 admin.site.register(Entity, EntityModelAdmin)
+admin.site.register(Feedback, FeedbackModelAdmin)
