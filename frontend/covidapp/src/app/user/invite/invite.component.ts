99fed16984658b59d49926f4aba5239733fe2603
Rajesh Golani <rajeshgolani@gmail.com>
2020-04-05 21:41:01 +0530

    Created user folder to put all the necessary user related components

---
+++ frontend/covidapp/src/app/user/invite/invite.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from "../../services/auth.service";
+
+@Component({
+  selector: 'app-invite',
+  templateUrl: './invite.component.html',
+  styleUrls: ['./invite.component.css']
+})
+export class InviteComponent implements OnInit {
+  email:string;
+  errorMessage:string;
+  error:boolean=false;
+  success:boolean=false;
+  message:string;
+  constructor( private authService:AuthService) { }
+
+  ngOnInit() {
+  }
+
+  onSubmit(){
+      this.message = "Invitation in process please wait...";
+      this.authService.invite({'email': this.email})
+        .subscribe(
+          data => {
+                  console.log('Reset Successful', data);
+		  this.message='';
+                  this.success=true;
+		  this.error=false;
+          },
+          err => {
+            console.error('error', err);
+	    this.errorMessage=this.authService.getErrorMessage(err);
+            this.success=false;
+	    this.error=true;
+          }
+
+      );
+
+
+      console.log(this.email);
+   }
+}
---
+++ frontend/covidapp/src/app/user/user-add/user-add.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router"
+import { AuthService } from "../../services/auth.service";
+import { UserService } from "../../services/user.service";
+
+@Component({
+  selector: 'app-user-add',
+  templateUrl: './user-add.component.html',
+  styleUrls: ['./user-add.component.css']
+})
+export class UserAddComponent implements OnInit {
+  username:string;
+  email:string;
+  password:string;
+  password2:string;
+  is_staff:boolean=false;
+  is_superuser:boolean=false
+  success:boolean=false;
+  errorMessage:string="";
+  constructor(public authService:AuthService, private userService:UserService, private router: Router) {
+  }
+  ngOnInit() {
+  }
+
+  onRegister(){
+      console.log(this.username);
+      console.log(this.authService.isAdmin());
+      this.userService.userCreate({'name': this.username,'email': this.email, 'password': this.password, 'password2': this.password2 ,'is_staff':this.is_staff})
+      .subscribe(
+        data => {
+                console.log('register success', data);
+                this.success=true;
+                setTimeout(() => {
+                  this.router.navigate(['/users/']);
+                }, 1000);
+        },
+         err => {
+                console.log(err.error);
+                this.success=false;
+                this.errorMessage=this.authService.getErrorMessage(err);
+             }
+      );
+  }
+}
+
+
