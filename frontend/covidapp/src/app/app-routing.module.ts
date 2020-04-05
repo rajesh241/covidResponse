7c0881707d1b1cd0b2667d1fe6cc160f490903a3
Rajesh Golani <rajeshgolani@gmail.com>
2020-04-05 12:46:19 +0530

    moved all the user modules to user directory

--- frontend/covidapp/src/app/app-routing.module.ts
+++ frontend/covidapp/src/app/app-routing.module.ts
@@ -12,18 +12,20 @@ import { ContextFilterComponent } from "./context/context-filter/context-filter.
 import { ContextEditComponent } from "./context/context-edit/context-edit.component";
 import { ContextMapComponent } from "./context/context-map/context-map.component";
 import { AboutComponent } from "./about/about.component";
-import { UserListComponent } from "./user-list/user-list.component";
-import { LoginComponent } from "./login/login.component";
-import { UserEditComponent } from "./user-edit/user-edit.component";
-import { UserAddComponent } from "./user-add/user-add.component";
 import { AvatarEditComponent } from "./avatar-edit/avatar-edit.component";
-import { ProfileEditComponent } from "./profile-edit/profile-edit.component";
-import { RegisterComponent } from "./register/register.component";
-import { RegisterConfirmComponent } from "./register-confirm/register-confirm.component";
-import { PasswordresetComponent } from "./passwordreset/passwordreset.component";
-import { PasswordresetConfirmComponent } from "./passwordreset-confirm/passwordreset-confirm.component";
-import { InviteComponent } from "./invite/invite.component";
 import { Oauth2callbackComponent } from './oauth2callback/oauth2callback.component';
+
+
+import { LoginComponent } from './user/login/login.component';
+import { UserListComponent } from './user/user-list/user-list.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { UserAddComponent } from './user/user-add/user-add.component';
+import { RegisterComponent } from './user/register/register.component';
+import { RegisterConfirmComponent } from './user/register-confirm/register-confirm.component';
+import { PasswordresetComponent } from './user/passwordreset/passwordreset.component';
+import { PasswordresetConfirmComponent } from './user/passwordreset-confirm/passwordreset-confirm.component';
+import { InviteComponent } from './user/invite/invite.component';
+import { ProfileEditComponent } from './user/profile-edit/profile-edit.component';
 
 const routes: Routes = [
     {path : '', component : CovidSearchComponent},
