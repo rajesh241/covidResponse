15d79dc3daeaefa8aa5037e9c42d4b69a41bae88
Rajesh Golani <rajeshgolani@gmail.com>
2020-04-23 16:33:51 +0530

    Added new component for my entity list

---
+++ frontend/covidapp/src/app/entity/my-entity-list/my-entity-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-entity-list',
+  templateUrl: './my-entity-list.component.html',
+  styleUrls: ['./my-entity-list.component.css']
+})
+export class MyEntityListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
