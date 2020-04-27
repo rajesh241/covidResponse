70785eaa4f1a82002e8a2307cf889a6afb5f5e5a
mynk <mr.mynk@gmail.com>
2020-04-28 02:42:46 +0530

    Add # to fix the url issue. Version along with Footer

--- frontend/covidapp/src/app/app.component.ts
+++ frontend/covidapp/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from "./services/auth.service";
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'toptalapp';
+  title = 'COAST India';
   constructor(public authService:AuthService) {
    
   }
