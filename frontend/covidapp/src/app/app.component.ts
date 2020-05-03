080ce0625b00f107de141f7a0a844c3e8d7aa38b
Mayank Rungta <mr.mynk@gmail.com>
2020-05-03 14:52:36 +0530

    Limiting version check for production only to avoid dev issues

--- frontend/covidapp/src/app/app.component.ts
+++ frontend/covidapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
 import { AuthService } from './services/auth.service';
 import { EntityService } from './services/entity.service';
 import { VERSION } from '../environments/version';
@@ -16,6 +16,8 @@ export class AppComponent implements OnInit {
     public github_url: string = `https://github.com/rajesh241/covidResponse/`;
     public commit_url: string;
     public version_str: string;
+    private subscriber;
+    private prevSubscriber;
 
     constructor(
 	public entityService: EntityService,
@@ -27,28 +29,37 @@ export class AppComponent implements OnInit {
 
     ngOnInit() {
 	console.log(`AppComponent.ngOnInit(${this.version}-${this.hash} Build[${this.build}])`);
-	const frequency = 1000 * 60 * 30;
-	this.entityService.getVersion()
-	    .subscribe(version => {
-		console.log(`AppComponent.ngOnInit.subscription(SERVER Version: ${JSON.stringify(version)})`);
-
-		this.commit_url = version.commit_url;
-		this.version_str = `${this.version}-${this.hash}`
-
-		setInterval(() => {
-		    this.versionCheck(version);
-		}, frequency);
-		this.versionCheck(version);
-	    });
-
+	// Set the version checker at periodic intervals
+	if(!isDevMode())
+	    this.setUpVersionChecker();
     }
 
-    versionCheck(version) {
-	console.log(`AppComponent.versionCheck(Version: ${this.version_str})`);
-	if (version.hash != this.hash) {
-	    console.log(`AppComponent.versionCheck(${this.version}-${this.hash} Build[${this.build}]) does not match ${this.version_str}`);
-	    // window.alert(`New Version Available [${this.version_str}]. Will reload page`);
-	    // window.location.reload(true);
+    setUpVersionChecker() {
+	const frequency = 1000 * 60 * 30; // 30 mins
+	setInterval(() => {
+	    this.versionCheck();
+	}, frequency);
+	this.versionCheck();
+    }
+
+    versionCheck() {
+	this.subscriber = this.entityService.getVersion()
+	    .subscribe(version => {
+		console.log(`AppComponent.versionCheck().subscription(SERVER Version: ${JSON.stringify(version)})`);
+
+		this.commit_url = version.commit_url;
+		this.version_str = `${this.version}-${version.hash}`
+
+		if (version.hash != this.hash) {
+		    console.log(`AppComponent.versionCheck(${this.version}-${this.hash} Build[${this.build}]) does not match ${this.version_str}`);
+		    window.alert(`New Version Available [${this.version_str}]. Will reload page`);
+		    window.location.reload(true);
+		}
+	    });
+
+	if (this.prevSubscriber) {
+	    this.prevSubscriber.unsubscribe();
 	}
+	this.prevSubscriber = this.subscriber;
     }
 }
