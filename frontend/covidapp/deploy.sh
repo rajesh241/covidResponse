1d48e996da15e45b62d098973f9afc9444fb5396
Mayank Rungta <mr.mynk@gmail.com>
2020-05-05 17:15:34 +0530

    Fixing the post-build. The version needs to be generated before the prod build

--- frontend/covidapp/deploy.sh
+++ frontend/covidapp/deploy.sh
@@ -1,5 +1,5 @@
-ng build --prod # --verbose=true --progress=true
-npm run post-build
+# ng build --prod # --verbose=true --progress=true
+# npm run post-build
 # Combines the above two commands
-# npm run build  # This will do versioning as well
+npm run build  # This will do versioning as well
 sudo cp -Rv dist/covidapp /var/www/html/angular/
