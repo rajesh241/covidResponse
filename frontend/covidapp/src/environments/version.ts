fb308773c90619cae571c42e147e4df4cd469fff
Mayank Rungta <mr.mynk@gmail.com>
2020-05-06 13:47:30 +0530

    Adding to avoid hassles during fresh pull and run

---
+++ frontend/covidapp/src/environments/version.ts
@@ -0,0 +1,15 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "1d48e99-dirty",
+    "hash": "1d48e99",
+    "distance": null,
+    "tag": null,
+    "semver": null,
+    "suffix": "1d48e99-dirty",
+    "semverString": null,
+    "version": "1.0.9",
+    "build": "36cef774b607b29dcc55"
+};
+/* tslint:enable */
