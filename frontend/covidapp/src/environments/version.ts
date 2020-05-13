1c72794f27df45d9bce8f66bd35478580747bb9c
Mayank Rungta <mr.mynk@gmail.com>
2020-05-14 00:43:03 +0530

    Preparing for a release. Org filter fixed and Export fields updated

--- frontend/covidapp/src/environments/version.ts
+++ frontend/covidapp/src/environments/version.ts
@@ -2,14 +2,14 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "4094970-dirty",
-    "hash": "4094970",
+    "raw": "6ecec7a-dirty",
+    "hash": "6ecec7a",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "4094970-dirty",
+    "suffix": "6ecec7a-dirty",
     "semverString": null,
-    "version": "1.1.2",
-    "build": "82ea6de08085c54e4d13"
+    "version": "1.1.4",
+    "build": "a40b2b8f3cb595c5f922"
 };
 /* tslint:enable */
